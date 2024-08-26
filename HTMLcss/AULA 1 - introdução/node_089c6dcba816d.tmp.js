71478155dbed3d1ab7ed7af058711e6d5140262e
euhsds <103261850+euhsds@users.noreply.github.com>
2024-08-26 11:27:46 -0300

    aula 1
    OLA MUNDO, MATERIAL SALA DE AULA.

---
+++ HTMLcss/AULA 1 - introdução/node_089c6dcba816d.tmp.js
@@ -0,0 +1,41 @@
+<!-- doctype é a versão do html 5-->
+<!DOCTYPE html>
+<!--lang é a liguagem da nossa página-->
+<html lang="en">
+  <head>
+    <!-- denifinir protocolo de caracteres -->
+    <meta charset="UTF-8" />
+    <!-- pegar as resoluções do dispositivo ( importante para @media) -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- titulo na aba da página -->
+    <title>AULA 1</title>
+  </head>
+  <body>
+    <!-- Olá mundo -->
+    <h1 style="color: blue"></h1>
+    <p>OlÁ MUNDO</p>
+    <p style="">TITULO MUITO BACANA</p>
+    <img src="01-guina-nationalgeographic_2750749.jpg" width="600px" />
+
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16742.087359562687!2d-48.05578015691116!3d-15.849218026739349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a33efa64a1cdd%3A0x88c03098a4846d51!2sAdesivo%20Geral%20DF!5e0!3m2!1spt-BR!2sbr!4v1724681262258!5m2!1spt-BR!2sbr"
+      width="600"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/ndAQfTzlVjc?si=La0McHwiKrspKJcI"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  </body>
+</html>
